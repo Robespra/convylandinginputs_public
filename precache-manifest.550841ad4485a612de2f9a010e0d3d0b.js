b56d382ab517c43bc0be18f05dd2aa67355ae3c5
robespra <robespra@gmail.com>
2020-11-30 12:18:26 +0000

    first commit

---
+++ precache-manifest.550841ad4485a612de2f9a010e0d3d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f18ada63527f4c3ff3c1df7bfa4bde6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7033af6608a4a1cdf14",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "92da16df21d6983a2b0c",
+    "url": "./static/js/2.93173b42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.93173b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7033af6608a4a1cdf14",
+    "url": "./static/js/main.2bc0c296.chunk.js"
+  },
+  {
+    "revision": "8949609e2e5e484d033e",
+    "url": "./static/js/runtime-main.d7622ae1.js"
+  }
+]);
